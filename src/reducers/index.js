6b45a44f3b1a59e89a98edd91eb1e6b5ff3e6078
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-11-05 10:04:32 -0700

    Reducer file updated; action constants created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,41 @@
+import { FETCH_START, FETCH_SUCCESS, FETCH_ERROR, ADD_SMURF } from '../actions/';
+
 
 export const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: ''
+
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_START:
+            return ({
+                ...state,
+                isLoading: true,
+                error: ''
+            });
+        case FETCH_SUCCESS:
+            return ({
+                smurfs: action.payload,
+                isLoading: false,
+                error: ''
+            }); 
+        case FETCH_ERROR:
+            return ({
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }); 
+        case ADD_SMURF:
+            return ({
+                ...state,
+                smurfs: [...state.smurfs, action.payload]
+            })
+        default:
+            return state;
+    }
 }
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
